71b83bf28e42e667c326482c6ab421730df5a154
gaurav <imgauravchandani@gmail.com>
2022-02-20 20:22:08 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,18 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={newsApi}>
+          Hit api
+        </button>
       </header>
     </div>
   );
+}
+
+const newsApi = () => {
+  fetch("https://newsapi.org/v2/top-headlines?apiKey=c97b1cbf02864ca2a687b0efd846de2b&category=general", {
+    method: "GET"
+  }).then(res => res.json()).then(data=>console.log(data));
 }
 
 export default App;
