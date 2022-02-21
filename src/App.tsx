58aa7fac61655166d27cdb322b5cc22011e9f317
gaurav <imgauravchandani@gmail.com>
2022-02-21 21:04:00 +0530

    changes: inshorts api test on server

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
 }
 
 const newsApi = () => {
-  fetch("https://newsapi.org/v2/top-headlines?apiKey=c97b1cbf02864ca2a687b0efd846de2b&category=general", {
+  fetch("https://inshortsapi.vercel.app/news?category=national", {
     method: "GET"
   }).then(res => res.json()).then(data=>console.log(data));
 }
